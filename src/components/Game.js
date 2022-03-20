1822cee76a0edcf7955799324ad15bb5dbd4dc69
kirtleyadams <hello@kirtleymichelle.com>
2022-03-19 23:06:38 -0400

    boxes

---
+++ src/components/Game.js
@@ -0,0 +1,42 @@
+import {Component} from 'react';
+import Board from './Board';
+import './Game.css';
+
+export default class Game extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            xIsNext: true,
+            stepNumber: 0,
+            history: [
+                {squares: Array(9).fill(null)}
+            ]
+        }
+    }
+    handleClick(i) {
+        const history = this.state.history.slice(0,this.state.stepNumber+1);
+        const current = history[history.length-1];
+        const squares = current.squares.slices();
+        squares[i] = this.state.xIsNext?'x': 'O';
+        this.setState({
+        history: history.concat({
+                squares: squares
+            }),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
+        })
+    }
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board onClick={(i) =>this.handleClick(i)}
+                    squares = {current.squares} />
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import './Square.css';
+
+export default function Square(props) {
+    return (
+        <button className='square' onClick={props.onClick}>
+        {props.value}
+        </button>
+    )
+}
